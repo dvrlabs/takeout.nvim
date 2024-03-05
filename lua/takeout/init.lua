1c88e70712a28b54b9ebb7d5cca8942bc06a4e46
dvr <dvrlabs.tv@gmail.com>
2024-03-05 06:01:27 -0500

    restructured functions

--- lua/takeout/init.lua
+++ lua/takeout/init.lua
@@ -12,7 +12,7 @@ local last_command = nil
 
 -- Enhanced 'bag' function to replace 'vim.keymap.set'
 -- It sets the keymap and makes the command repeatable
-function M.bag(mode, lhs, rhs, keymap_opts)
+M.bag = function(mode, lhs, rhs, keymap_opts)
     keymap_opts = keymap_opts or {}
     local callback
 
@@ -35,16 +35,16 @@ function M.bag(mode, lhs, rhs, keymap_opts)
 end
 
 -- Function to repeat the last command
-function M.repeat_last_command()
+M.repeat_last_command = function()
     if last_command then
         last_command()
     end
 end
 
 -- Setup function to configure the plugin and set the repeat key
-function M.setup(user_opts)
-    user_opts = user_opts or {}
-    for k, v in pairs(user_opts) do
+M.setup = function(opts)
+    opts = opts or {}
+    for k, v in pairs(opts) do
         if opts[k] ~= nil then
             opts[k] = v
         end
@@ -54,7 +54,7 @@ function M.setup(user_opts)
 end
 
 -- Function to set or update the repeat key mapping
-function M.set_repeat_key()
+M.set_repeat_key = function()
     -- Set the new key mapping for repeating the last command
     vim.keymap.set('n', opts.repeat_key, M.repeat_last_command, { noremap = true, silent = true })
 end
